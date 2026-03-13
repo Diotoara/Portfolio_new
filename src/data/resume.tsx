627ff2e1d4ca8e18b2c86d846af3ad11ea9354ba
diotoara <aapka.5ryan@gmail.com>
2026-03-14 01:27:48 +0530

    video error resolved in projects

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -139,7 +139,7 @@ export const DATA = {
         },
       ],
       image: "./linkomatic.png",
-      // video:"https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
+      video:"",
     },
     {
       title: "Chat App",
